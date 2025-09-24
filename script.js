ab921155773c5715a64f505b60dd0a35745dbfe3
Esdras Mora <150874104+esdras04@users.noreply.github.com>
2025-09-24 21:04:45 +0000

    added progress bar functionality and greeting message

--- script.js
+++ script.js
@@ -22,20 +22,34 @@ form.addEventListener('submit', function(e){
   console.log(`Name: ${name}, Team: ${teamName}`);
 
   //increment count 
+   const teamCounter = document.getElementById(teamValue + 'Count');
   count++;
+
+  if (count<= maxCapacity){
+
+    attendeeCount.textContent = count;
+  //update team counter
+    teamCounter.textContent = parseInt(teamCounter.textContent) +1;
+  }
+    
+
   console.log(`Total Attendance: ${count}`);
+  console.log(teamCounter);
 
   //update progress bar
   const percentage = Math.round((count / maxCapacity) * 100) + '%';
   console.log(`Attendance: ${percentage}`);
 
-  //update team counter
-  const teamCounter = document.getElementById(teamValue + 'Count');
- teamCounter.textContent = parseInt(teamCounter.textContent) +1;
-  console.log(teamCounter);
+  const progressBar = document.getElementById('progressBar');
+  progressBar.style.width = percentage;
+
+
 
   //welcome message 
   const message = `Welcome ${name} from ${teamName}! You are attendee number ${count}.`;
+  document.getElementById("greeting").textContent = message;
+  greeting.style.display = 'block';
+  console.log(message);
 
   form.reset();
 });
